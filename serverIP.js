0774f7c2012a41277903376362df3701d8670236
vikeshvaghela <vikesh.radhecreation@gmail.com>
2018-01-13 16:59:10 +0530

    gggtegerg

---
+++ serverIP.js
@@ -0,0 +1,16 @@
+//
+//
+//  PRINTS THE SERVER IP ADDRESS
+//
+//
+var os=require('os');
+var ifaces=os.networkInterfaces();
+for (var dev in ifaces) {
+  var alias=0;
+  ifaces[dev].forEach(function(details){
+    if (details.family=='IPv4') {
+      console.log(dev+(alias?':'+alias:''),details.address);
+      ++alias;
+    }
+  });
+}
